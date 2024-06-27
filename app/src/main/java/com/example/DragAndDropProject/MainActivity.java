bbb3fa137befe3dbd400f98034deb9816d63ac4f
Juri <juri.kaemper@googlemail.com>
2024-06-27 13:46:22 +0200

    add years and events; also refactor a lot of code

--- app/src/main/java/com/example/DragAndDropProject/MainActivity.java
+++ app/src/main/java/com/example/DragAndDropProject/MainActivity.java
@@ -16,128 +16,84 @@ public class MainActivity extends AppCompatActivity {
 
     private List<TextView> dragItems = new ArrayList<>();
     private List<TextView> dropAreas = new ArrayList<>();
-    private List<String> originalDropAreaTexts = new ArrayList<>();
     private Button showAnswerButton;
     private Button resetButton;
     private boolean[] isAnswered;
     private boolean[] isAnsweredCorrectly;
+    // Define array for drag item texts
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize your drag items
-        dragItems.add((TextView) findViewById(R.id.dragItem1));
-        dragItems.add((TextView) findViewById(R.id.dragItem2));
-        dragItems.add((TextView) findViewById(R.id.dragItem3));
-        dragItems.add((TextView) findViewById(R.id.dragItem4));
-        dragItems.add((TextView) findViewById(R.id.dragItem5));
-        dragItems.add((TextView) findViewById(R.id.dragItem6));
-        dragItems.add((TextView) findViewById(R.id.dragItem7));
-        dragItems.add((TextView) findViewById(R.id.dragItem8));
+        String[] dragItemTexts = {
+                "Founding of Rome",
+                "America is discovered",
+                "Protestant Reformation",
+                "American Revolution",
+                "French Revolution",
+                "End of World War II",
+                "The Moon Landing",
+                "Fall of the Berlin Wall"
+        };
 
-        // Initialize your drop areas and store original texts
-        TextView dropArea1 = (TextView) findViewById(R.id.dropArea1);
-        dropAreas.add(dropArea1);
-        originalDropAreaTexts.add(dropArea1.getText().toString());
+        String[] dropAreaTexts = {
+                "753 BCE",
+                "1492",
+                "1517",
+                "1776",
+                "1789",
+                "1945",
+                "1969",
+                "1989"
+        };
 
-        TextView dropArea2 = (TextView) findViewById(R.id.dropArea2);
-        dropAreas.add(dropArea2);
-        originalDropAreaTexts.add(dropArea2.getText().toString());
+        // Initialize your drag items dynamically
+        int[] dragItemIds = {
+                R.id.dragItem1,
+                R.id.dragItem2,
+                R.id.dragItem3,
+                R.id.dragItem4,
+                R.id.dragItem5,
+                R.id.dragItem6,
+                R.id.dragItem7,
+                R.id.dragItem8
+        };
 
-        TextView dropArea3 = (TextView) findViewById(R.id.dropArea3);
-        dropAreas.add(dropArea3);
-        originalDropAreaTexts.add(dropArea3.getText().toString());
+        for (int i = 0; i < dragItemIds.length; i++) {
+            TextView dragItem = findViewById(dragItemIds[i]);
+            dragItem.setText(dragItemTexts[i]);
+            dragItems.add(dragItem);
+        }
 
-        TextView dropArea4 = (TextView) findViewById(R.id.dropArea4);
-        dropAreas.add(dropArea4);
-        originalDropAreaTexts.add(dropArea4.getText().toString());
+        // Initialize your drop areas with fixed texts
+        int[] dropAreaIds = {
+                R.id.dropArea1,
+                R.id.dropArea2,
+                R.id.dropArea3,
+                R.id.dropArea4,
+                R.id.dropArea5,
+                R.id.dropArea6,
+                R.id.dropArea7,
+                R.id.dropArea8
+        };
 
-        TextView dropArea5 = (TextView) findViewById(R.id.dropArea5);
-        dropAreas.add(dropArea5);
-        originalDropAreaTexts.add(dropArea5.getText().toString());
-
-        TextView dropArea6 = (TextView) findViewById(R.id.dropArea6);
-        dropAreas.add(dropArea6);
-        originalDropAreaTexts.add(dropArea6.getText().toString());
-
-        TextView dropArea7 = (TextView) findViewById(R.id.dropArea7);
-        dropAreas.add(dropArea7);
-        originalDropAreaTexts.add(dropArea7.getText().toString());
-
-        TextView dropArea8 = (TextView) findViewById(R.id.dropArea8);
-        dropAreas.add(dropArea8);
-        originalDropAreaTexts.add(dropArea8.getText().toString());
+        for (int i = 0; i < dropAreaIds.length; i++) {
+            TextView dropArea = findViewById(dropAreaIds[i]);
+            dropArea.setText(dropAreaTexts[i]);
+            dropAreas.add(dropArea);
+        }
 
         // Initialize isAnswered array based on the number of drop areas
         isAnswered = new boolean[dropAreas.size()];
         isAnsweredCorrectly = new boolean[dropAreas.size()];
 
-        // Set up drag listeners for each drag item
-        for (int i = 0; i < dragItems.size(); i++) {
-            final int index = i;
-            dragItems.get(i).setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    View.DragShadowBuilder myShadow = new View.DragShadowBuilder(dragItems.get(index));
-                    // Start the drag.
-                    view.startDragAndDrop(null,  // The data to be dragged.
-                            myShadow,                 // The drag shadow builder.
-                            dragItems.get(index),     // No need to use local data.
-                            0                         // Flags. Not currently used, set to 0.
-                    );
+        // Set up drag item listeners for each drag item
+        setUpDragItemListeners();
 
-                    return false;
-                }
-            });
-        }
-
-        // Set up drag listeners for each drop area
-        for (int i = 0; i < dropAreas.size(); i++) {
-            final int index = i;
-
-            dropAreas.get(i).setOnDragListener(new View.OnDragListener() {
-                @Override
-                public boolean onDrag(View view, DragEvent dragEvent) {
-                    TextView draggedView = (TextView) dragEvent.getLocalState();
-
-                    switch (dragEvent.getAction()) {
-                        case DragEvent.ACTION_DRAG_STARTED:
-                            return true;
-                        case DragEvent.ACTION_DRAG_ENTERED:
-                            if (!isAnswered[index]) {
-                                view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.fuschia));
-                            }
-                            return true;
-                        case DragEvent.ACTION_DRAG_EXITED:
-                            if (!isAnswered[index]) {
-                                view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.holo_blue_light));
-                            }
-                            return true;
-                        case DragEvent.ACTION_DROP:
-                            view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.holo_blue_dark));
-
-                            String dropAreaText = dropAreas.get(index).getText().toString();
-                            String dragItemText = draggedView.getText().toString();
-
-                            // Concatenate dropAreaText and dragItemText with a line break
-                            String combinedText = dropAreaText + ":\n" + dragItemText;
-
-                            dropAreas.get(index).setText(combinedText);
-
-                            isAnswered[index] = true;
-                            isAnsweredCorrectly[index] = draggedView.getId() == dragItems.get(index).getId();
-                            checkAllAnswered(); // Check if all answers are made
-                            return true;
-                        case DragEvent.ACTION_DRAG_ENDED:
-                            return true;
-                        default:
-                            return false;
-                    }
-                }
-            });
-        }
+        // Set up drop area listeners for each drop area
+        setUpDropAreaListeners();
 
         // Initialize showAnswerButton and set onClickListener
         showAnswerButton = findViewById(R.id.showButton);
@@ -167,15 +123,21 @@ public class MainActivity extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Show answers (red/green colors)
                 for (int i = 0; i < dropAreas.size(); i++) {
                     TextView dropArea = dropAreas.get(i);
                     dropArea.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.holo_blue_light));
-                    dropArea.setText(originalDropAreaTexts.get(i));  // Reset text to original
+                    dropArea.setText(dropAreaTexts[i]);  // Reset text to original
                     isAnswered[i] = false;
                     isAnsweredCorrectly[i] = false;
                     showAnswerButton.setEnabled(false);
+
+                    TextView dragItem = dragItems.get(i);
+                    dragItem.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.holo_green_light));
                 }
+                // Re-Enable the OnLongClickListeners
+                setUpDragItemListeners();
+                // Re-Enable the OnDragListeners
+                setUpDropAreaListeners();
             }
         });
     }
@@ -184,10 +146,85 @@ public class MainActivity extends AppCompatActivity {
     private void checkAllAnswered() {
         for (boolean answered : isAnswered) {
             if (!answered) {
+                showAnswerButton.setEnabled(true); //TODO REMOVE
                 return; // Exit if any answer is not made
             }
         }
         // Enable the button if all answers are made
         showAnswerButton.setEnabled(true);
+    }
+
+    // Method to set up drag item listeners for each drag item
+    private void setUpDragItemListeners() {
+        for (int i = 0; i < dragItems.size(); i++) {
+            final int index = i;
+            dragItems.get(i).setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    View.DragShadowBuilder myShadow = new View.DragShadowBuilder(dragItems.get(index));
+                    // Start the drag.
+                    view.startDragAndDrop(null,  // The data to be dragged.
+                            myShadow,                 // The drag shadow builder.
+                            dragItems.get(index),     // No need to use local data.
+                            0                         // Flags. Not currently used, set to 0.
+                    );
+
+                    return false;
+                }
+            });
+        }
+    }
+
+    // Method to set up drop area listeners for each drop area
+    private void setUpDropAreaListeners() {
+        for (int i = 0; i < dropAreas.size(); i++) {
+            final int index = i;
+
+            dropAreas.get(i).setOnDragListener(new View.OnDragListener() {
+                @Override
+                public boolean onDrag(View view, DragEvent dragEvent) {
+                    TextView draggedView = (TextView) dragEvent.getLocalState();
+
+                    switch (dragEvent.getAction()) {
+                        case DragEvent.ACTION_DRAG_STARTED:
+                            return true;
+                        case DragEvent.ACTION_DRAG_ENTERED:
+                            if (!isAnswered[index]) {
+                                view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.holo_blue_dark));
+                            }
+                            return true;
+                        case DragEvent.ACTION_DRAG_EXITED:
+                            if (!isAnswered[index]) {
+                                view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.holo_blue_light));
+                            }
+                            return true;
+                        case DragEvent.ACTION_DROP:
+                            view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.holo_blue_dark));
+                            draggedView.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.green_assigned));
+
+                            // Disable OnLongClickListener
+                            draggedView.setOnLongClickListener(null);
+                            view.setOnDragListener(null);
+
+                            String dropAreaText = dropAreas.get(index).getText().toString();
+                            String dragItemText = draggedView.getText().toString();
+
+                            // Concatenate dropAreaText and dragItemText with a line break
+                            String combinedText = dropAreaText + ":\n" + dragItemText;
+
+                            dropAreas.get(index).setText(combinedText);
+
+                            isAnswered[index] = true;
+                            isAnsweredCorrectly[index] = draggedView.getId() == dragItems.get(index).getId();
+                            checkAllAnswered(); // Check if all answers are made
+                            return true;
+                        case DragEvent.ACTION_DRAG_ENDED:
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            });
+        }
     }
 }
